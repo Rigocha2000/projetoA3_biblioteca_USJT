9c066767c7ab7b770bd3e8dc2d60eb61bd70c2d9
Guilherme Rocha <guirocha_2003@hotmail.com>
2023-10-08 10:46:35 -0300

    Criei o arquivo TelaLogin

---
+++ LoginGitTela/src/main/java/com/mycompany/logingittela/LoginGitTela.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.logingittela;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class LoginGitTela {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
